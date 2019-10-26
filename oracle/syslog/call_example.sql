bece2ce5b2b775ef929aa1b171578010e596fce7
Maarten Smeets <MaartenSmeets@users.noreply.github.com>
2019-10-26 14:39:43 +0200

    Adding example to call the SYSLOGGER

---
+++ oracle/syslog/call_example.sql
@@ -0,0 +1,28 @@
+//in /etc/rsyslog.conf uncomment
+//$ModLoad imudp
+//$UDPServerRun 514
+DECLARE
+  P_HOSTNAME VARCHAR2(200);
+  P_PORT NUMBER;
+  P_IDENT VARCHAR2(200);
+  P_FACILITY NUMBER;
+  P_PRIORITY NUMBER;
+  P_MSG VARCHAR2(200);
+BEGIN
+  P_HOSTNAME := NULL;
+  P_PORT := NULL;
+  P_IDENT := 'Syslogtest';
+  P_FACILITY := NULL;
+  P_PRIORITY := 1;
+  P_MSG := 'Hi there';
+
+  SYSLOGGER(
+    P_HOSTNAME => P_HOSTNAME,
+    P_PORT => P_PORT,
+    P_IDENT => P_IDENT,
+    P_FACILITY => P_FACILITY,
+    P_PRIORITY => P_PRIORITY,
+    P_MSG => P_MSG
+  );
+--rollback; 
+END;
